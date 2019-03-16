2961b07069ddf73da53ee59fd3c96fe22c670fb1
visualstudio <visualstudio@122-PC29>
2019-03-15 20:02:11 -0400

    final commit

--- BankAccountNS/Program.cs
+++ BankAccountNS/Program.cs
@@ -3,16 +3,23 @@
 //shingarjeet singh(c0731902)
 namespace BankAccountNS
 {
+    /// <summary>
+    /// Bank Account demo class.
+    /// </summary>
     public class BankAccount
     {
+        public const string DebitAmountExceedsBalanceMessage = "Debit amount exceeds balance";
+        public const string DebitAmountLessThanZeroMessage = "Debit amount is less than Zero";
         private string m_customerName;
+
         private double m_balance;
+
         private bool m_frozen = false;
 
         private BankAccount()
         {
-
         }
+
         public BankAccount(string customerName, double balance)
         {
             m_customerName = customerName;
@@ -20,10 +27,10 @@ namespace BankAccountNS
         }
 
         public string CustomerName
-
         {
             get { return m_customerName; }
         }
+
         public double Balance
         {
             get { return m_balance; }
@@ -31,26 +38,17 @@ namespace BankAccountNS
 
         public void Debit(double amount)
         {
-            if (m_frozen)
-
-            {
-                throw new Exception("Account frozen");
-
-            }
-
             if (amount > m_balance)
-
             {
-                throw new ArgumentOutOfRangeException("amount");
-
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountExceedsBalanceMessage);
             }
+
             if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException("amount");
+                throw new ArgumentOutOfRangeException("amount", amount, DebitAmountLessThanZeroMessage);
             }
 
-            m_balance -= amount;
-
+            m_balance -= amount; // intentionally incorrect code
         }
 
         public void Credit(double amount)
@@ -62,23 +60,20 @@ namespace BankAccountNS
 
             if (amount < 0)
             {
-                throw new ArgumentOutOfRangeException(" AMOUNT");
+                throw new ArgumentOutOfRangeException("amount");
             }
 
             m_balance += amount;
-
         }
 
         private void FreezeAccount()
         {
             m_frozen = true;
-
         }
 
-        private void unFreezeAccount()
+        private void UnfreezeAccount()
         {
             m_frozen = false;
-
         }
 
         public static void Main()
@@ -86,8 +81,8 @@ namespace BankAccountNS
             BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
 
             ba.Credit(5.77);
-            ba.Credit(11.22);
-            Console.WriteLine("CURRENT BALANCE IS $ {0}", ba.Balance);
+            ba.Debit(11.22);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
         }
     }
 }
---
+++ BankTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8b2cfc48-51d2-4677-90f9-c6094be3c287")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
